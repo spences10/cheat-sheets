edc406613e278a8c240dc6382d8a2effa437f76e
spences10 <spences10apps@gmail.com>
2018-10-09 19:05:27 +0100

    add cheet sheet template

---
+++ src/templates/cheatSheetTemplate.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const cheatSheetPage = props => {
+  return (
+    <React.Fragment>
+      <p>jsx goes here</p>
+    </React.Fragment>
+  )
+}
+
+export default cheatSheetPage

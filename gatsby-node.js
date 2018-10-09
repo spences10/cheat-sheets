3b7fdd6a916f771f7d2757aef58f5c85be97a7b0
spences10 <spences10apps@gmail.com>
2018-10-09 20:51:21 +0100

    use node to populate sheets template

---
+++ gatsby-node.js
@@ -0,0 +1,55 @@
+const path = require('path')
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+  const cheatSheetTemplate = path.resolve(
+    'src/templates/cheatSheetTemplate.js'
+  )
+
+  // returns promise that will start with this graphql query
+  return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: ASC, fields: [frontmatter___updatedDate] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            html
+            id
+            frontmatter {
+              createdDate
+              updatedDate
+              path
+              title
+              description
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
+
+    const sheets = result.data.allMarkdownRemark.edges
+
+    // Create pages for each markdown file.
+    sheets.forEach(({ node }, index) => {
+      const prev = index === 0 ? null : sheets[index - 1].node
+      const next =
+        index === sheets.length - 1 ? null : sheets[index + 1].node
+      createPage({
+        path: node.frontmatter.path,
+        component: cheatSheetTemplate,
+        context: {
+          prev,
+          next
+        }
+      })
+    })
+
+    return sheets
+  })
+}

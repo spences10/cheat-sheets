42bae05d76dd7bbbb589c143db6f7059b8b3270f
spences10 <spences10apps@gmail.com>
2018-10-09 19:01:59 +0100

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,32 @@ module.exports = {
   siteMetadata: siteMetadata,
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components'
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/posts`,
+        name: 'posts'
+      }
+    },
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590
+            }
+          },
+          'gatsby-remark-autolink-headers',
+          'gatsby-remark-prismjs'
+        ]
+      }
+    }
   ]
 }

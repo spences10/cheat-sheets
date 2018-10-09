6c4253c1259bb7d2d1e13390683b1708e74630d1
spences10 <spences10apps@gmail.com>
2018-10-09 20:44:45 +0100

    add page data

--- src/templates/cheatSheetTemplate.js
+++ src/templates/cheatSheetTemplate.js
@@ -3,12 +3,33 @@ import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const cheatSheetPage = props => {
+const cheatSheetPage = ({ data /*, pageContext */ }) => {
+  const post = data.markdownRemark
+  // const { prev, next } = pageContext
+  // const { imageLink } = data.site.siteMetadata
   return (
     <Layout>
-      <p>jsx goes here</p>
+      <h1>{post.frontmatter.title}</h1>
+      <p>{post.frontmatter.createdDate}</p>
+      <p>{post.frontmatter.updatedDate}</p>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
     </Layout>
   )
 }
 
 export default cheatSheetPage
+
+export const query = graphql`
+  query($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      excerpt(pruneLength: 250)
+      frontmatter {
+        title
+        path
+        createdDate(formatString: "YYYY MMMM Do")
+        updatedDate(formatString: "YYYY MMMM Do")
+      }
+    }
+  }
+`

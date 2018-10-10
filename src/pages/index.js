ee55addd6e92add02989a56e3c176866c90a8325
spences10 <spences10apps@gmail.com>
2018-10-10 06:02:26 +0100

    query for main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import { graphql, Link } from 'gatsby'
 
-const Index = props => {
+import Layout from '../components/layout'
+
+export default ({ data }) => {
   return (
-    <React.Fragment>
-      <p>jsx goes here</p>
-    </React.Fragment>
+    <Layout>
+      {data.allMarkdownRemark.edges.map(({ node }, index) => (
+        <ul key={index}>
+          <Link to={node.frontmatter.path}>
+            <h1>{node.frontmatter.title}</h1>
+            <p>{node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </Link>
+        </ul>
+      ))}
+    </Layout>
   )
 }
 
-export default Index
+export const query = graphql`
+  query {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___createdDate], order: DESC }
+    ) {
+      totalCount
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            title
+            path
+            createdDate(formatString: "YYYY MMMM Do")
+          }
+        }
+      }
+    }
+  }
+`

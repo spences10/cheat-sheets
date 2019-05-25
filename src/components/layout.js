762ec17d693bcabaeb46f3959e0c112301af8e13
spences10 <spences10apps@gmail.com>
2019-05-25 19:50:23 +0100

    start layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 import Header from '../components/header';
 import { useSiteMetadata } from '../hooks/useSiteMetadata';
+
+const AppLayout = styled.main`
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: auto;
+  grid-template-areas:
+    'h h h h h h h h h h h h'
+    '. . . m m m m m m . . .'
+    'f f f f f f f f f f f f';
+`;
+
+const Wrapper = styled.div`
+  grid-area: m;
+`;
 
 export const Layout = ({ children }) => {
   const { title } = useSiteMetadata();
   return (
-    <>
+    <AppLayout>
       <Header title={title} />
-      {children}
-    </>
+      <Wrapper>{children}</Wrapper>
+    </AppLayout>
   );
 };

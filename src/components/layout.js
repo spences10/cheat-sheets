0b1481a65ec11663ac4131e33dc6e6838adc855a
spences10 <spences10apps@gmail.com>
2019-05-27 17:33:25 +0100

    amend layout for now

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,9 +9,9 @@ const AppLayout = styled.main`
   grid-template-columns: repeat(12, 1fr);
   grid-template-rows: auto;
   grid-template-areas:
-    'h h h h h h h h h h h h'
+    '. . . h h h h h h . . .'
     '. . . m m m m m m . . .'
-    'f f f f f f f f f f f f';
+    '. . . f f f f f f . . .';
 `;
 
 const Wrapper = styled.div`

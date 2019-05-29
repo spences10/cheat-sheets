6177e64374959706656be0ffc2f44d83ed501668
spences10 <spences10apps@gmail.com>
2019-05-29 21:55:29 +0100

    add sixes use in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { Header } from '../components/header';
 import { useSiteMetadata } from '../hooks/useSiteMetadata';
 import { GlobalStyle } from '../theme/globalStyle';
+import { media } from '../theme/sizes';
 
 const AppLayout = styled.main`
   display: grid;
@@ -13,6 +14,36 @@ const AppLayout = styled.main`
     '. . . h h h h h h . . .'
     '. . . m m m m m m . . .'
     '. . . f f f f f f . . .';
+  ${media.giant`
+    grid-template-areas:
+      '. . . h h h h h h . . .'
+      '. . . m m m m m m . . .'
+      '. . . f f f f f f . . .';
+    background: goldenrod;
+  `};
+  ${media.desktop`
+    grid-template-areas:
+      '. . . h h h h h h . . .'
+      '. . . m m m m m m . . .'
+      '. . . f f f f f f . . .';
+    background: dodgerblue;
+  `};
+  ${media.tablet`
+    grid-template-columns: repeat(9, 1fr);
+    grid-template-areas:
+      'h h h h h h h h h'
+      'm m m m m m m m m'
+      'f f f f f f f f f';
+    background: mediumseagreen;
+  `};
+  ${media.phone`
+    grid-template-columns: repeat(9, 1fr);
+    grid-template-areas:
+      'h h h h h h h h h'
+      'm m m m m m m m m'
+      'f f f f f f f f f';
+    background: palevioletred;
+  `};
 `;
 
 const Wrapper = styled.div`

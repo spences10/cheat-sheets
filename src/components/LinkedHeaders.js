5500bda3957a30595df884677f33b7aa3688ec05
spences10 <spences10apps@gmail.com>
2019-07-23 20:16:12 +0100

    linked header variable

---
+++ src/components/LinkedHeaders.js
@@ -0,0 +1,17 @@
+export const HeaderLink = `
+a {
+  float: left;
+  padding-right: 4px;
+  margin-left: -20px;
+}
+svg {
+  visibility: hidden;
+}
+  &: hover {
+  a {
+    svg {
+      visibility: visible;
+    }
+  }
+}
+`;

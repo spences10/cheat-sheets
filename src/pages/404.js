e237c65ff5ec33af7b1eabd64d4acbec22f3c3af
spences10 <spences10apps@gmail.com>
2019-09-30 06:37:11 +0100

    export default on 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,9 @@
 import React from 'react';
+import { Layout } from '../components/layout';
 import { H1 } from '../components/page-elements';
 
-const NotFoundPage = () => (
-  <div>
-    <H1>NOT FOUND</H1>
-    <p>You just hit a route that does not exist... the sadness.</p>
-  </div>
+export default () => (
+  <Layout>
+    <H1>That's a nope!</H1>
+  </Layout>
 );
-
-export default NotFoundPage;

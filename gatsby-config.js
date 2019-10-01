d898d7c3df61492eca75c9158c2956e14ff69bb1
Scott Spence <scott.spence@karmarama.com>
2019-10-01 06:38:37 +0100

    gatsby manifest config

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,5 +41,18 @@ module.exports = {
         anonymize: false,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `./static/favicon.gif`,
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 };

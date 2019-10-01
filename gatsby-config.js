72a0838e26b155f7427d666550d2b2c33d32c3d3
spences10 <spences10apps@gmail.com>
2019-10-01 07:08:48 +0100

    png

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon: `./static/favicon.gif`,
+        icon: `./static/favicon.png`,
       },
     },
     `gatsby-plugin-offline`,

cd3071bf67d76588892d2134b53348e926c6e949
spences10 <spences10apps@gmail.com>
2019-10-05 20:58:25 +0100

    geat title being marked

--- src/components/sheet-preview.js
+++ src/components/sheet-preview.js
@@ -1,27 +1,26 @@
 import { Link } from 'gatsby';
 import React from 'react';
 import { Highlight } from 'react-instantsearch-dom';
-import styled from 'styled-components';
 import { H1 } from './page-elements';
 
-const SheetsListWrapper = styled.main`
-  li {
-    list-style: none;
-  }
-`;
-
 export const SheetPreview = ({ hit }) => {
+  const headings = hit.headings
+    .map(h => {
+      return h.value;
+    })
+    .join(', ');
   return (
-    <SheetsListWrapper>
-      <Link to={hit.fields.slug}>
-        <H1>{hit.frontmatter.title}</H1>
-        {/* <Dump hit={hit} /> */}
-        <Highlight hit={hit} attribute="title" tagName="mark" />
-        {/* <span style={{ display: 'flex' }}>
-              <p>Created: {node.frontmatter.createdDate}</p>
-              <p>Updated: {node.frontmatter.updatedDate}</p>
-            </span> */}
-      </Link>
-    </SheetsListWrapper>
+    <Link to={hit.fields.slug}>
+      <H1>
+        <Highlight
+          hit={hit}
+          attribute="frontmatter.title"
+          tagName="mark"
+        />
+      </H1>
+      <p>
+        <Highlight hit={hit} attribute={headings} tagName="mark" />
+      </p>
+    </Link>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import {
   InstantSearch,
   SearchBox,
 } from 'react-instantsearch-dom';
+import styled from 'styled-components';
 import { Layout } from '../components/layout';
 import { SheetPreview } from '../components/sheet-preview';
 
@@ -13,13 +14,35 @@ const searchClient = algoliasearch(
   process.env.ALGOLIA_SEARCH_ONLY_KEY
 );
 
+const StyledHits = styled(Hits)`
+  li {
+    list-style: none;
+  }
+  margin: 0;
+  padding: 0;
+`;
+
+const Box = styled(SearchBox)`
+  input {
+    width: 100%;
+    height: 50px;
+    border-radius: 5px;
+    border: solid 1px lightgray;
+    font-size: 30px;
+    font-family: ${props => props.theme.h1};
+  }
+  button {
+    display: none;
+  }
+`;
+
 export default () => (
   <Layout>
     <InstantSearch
       searchClient={searchClient}
       indexName={process.env.ALGOLIA_INDEX_NAME}>
-      <SearchBox />
-      <Hits hitComponent={SheetPreview} />
+      <Box />
+      <StyledHits hitComponent={SheetPreview} />
     </InstantSearch>
 
     {/* <SheetsList /> */}

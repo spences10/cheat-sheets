be375a44e028e5f46ecadfa47e706870493ee91d
spences10 <spences10apps@gmail.com>
2019-10-06 16:31:54 +0100

    link herf on TOC

--- src/components/sheet-preview.js
+++ src/components/sheet-preview.js
@@ -4,11 +4,6 @@ import { Highlight } from 'react-instantsearch-dom';
 import { H1 } from './page-elements';
 
 export const SheetPreview = ({ hit }) => {
-  const hrefsArr = [];
-  hit.tableOfContents.items.map(i => {
-    return hrefsArr.push(i.url);
-  });
-
   return (
     <>
       <Link to={hit.fields.slug}>
@@ -21,51 +16,17 @@ export const SheetPreview = ({ hit }) => {
         </H1>
       </Link>
 
-      <p>
-        <Link to={hit.fields.slug + hrefsArr[0]}>
-          <Highlight
-            hit={hit}
-            attribute="tableOfContents.items[0].title"
-            tagName="mark"
-          />
-        </Link>
-      </p>
-      <p>
-        <Link to={hit.fields.slug + hrefsArr[1]}>
-          <Highlight
-            hit={hit}
-            attribute="tableOfContents.items[1].title"
-            tagName="mark"
-          />
-        </Link>
-      </p>
-      <p>
-        <Link to={hit.fields.slug + hrefsArr[2]}>
-          <Highlight
-            hit={hit}
-            attribute="tableOfContents.items[2].title"
-            tagName="mark"
-          />
-        </Link>
-      </p>
-      <p>
-        <Link to={hit.fields.slug + hrefsArr[3]}>
-          <Highlight
-            hit={hit}
-            attribute="tableOfContents.items[3].title"
-            tagName="mark"
-          />
-        </Link>
-      </p>
-      <p>
-        <Link to={hit.fields.slug + hrefsArr[4]}>
-          <Highlight
-            hit={hit}
-            attribute="tableOfContents.items[4].title"
-            tagName="mark"
-          />
-        </Link>
-      </p>
+      {hit.tableOfContents.items.map((item, index) => (
+        <p key={item.url}>
+          <Link to={hit.fields.slug + item.url}>
+            <Highlight
+              hit={hit}
+              attribute={`tableOfContents.items[${index}].title`}
+              tagName="mark"
+            />
+          </Link>
+        </p>
+      ))}
     </>
   );
 };

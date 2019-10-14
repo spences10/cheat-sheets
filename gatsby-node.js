c4d0077c805b77308fc7ce8993d49841c263f763
spences10 <spences10apps@gmail.com>
2019-10-14 19:36:04 +0100

    add things, nothings works

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const { createFilePath } = require(`gatsby-source-filesystem`);
+const { createPrinterNode } = require(`gatsby-plugin-printer`);
+const slugify = require(`@sindresorhus/slugify`);
 
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions;
@@ -60,6 +62,21 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       name: `slug`,
       node,
       value,
+    });
+
+    const printerNode = createPrinterNode({
+      id: node.id,
+      // fileName is something you can use in opengraph images, etc
+      fileName: slugify(node.title),
+      // renderDir is relative to `public` by default
+      outputDir: 'blog-post-images',
+      // data gets passed directly to your react component
+      data: node,
+      // the component to use for rendering. Will get batched with
+      // other nodes that use the same component
+      component: require.resolve(
+        './src/printer-components/blog-post.js'
+      ),
     });
   }
 };

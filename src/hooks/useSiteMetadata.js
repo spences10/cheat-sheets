8972d0a1f5f6d95335ccce6778334db3662c7915
spences10 <spences10apps@gmail.com>
2019-10-28 19:35:05 +0000

    add Author name

--- src/hooks/useSiteMetadata.js
+++ src/hooks/useSiteMetadata.js
@@ -14,6 +14,7 @@ export const useSiteMetadata = () => {
             description
             image
             twitterUsername
+            authorName
             faviconPng
             backgroundColour
             themeColour

ecf41389ac256f0314126f5d1874490ce0aa5012
spences10 <spences10apps@gmail.com>
2019-11-20 17:14:11 +0000

    strip out Algolia search

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,76 +1,18 @@
-import algoliasearch from 'algoliasearch/lite';
+import { graphql } from 'gatsby';
 import React from 'react';
-import {
-  Hits,
-  InstantSearch,
-  SearchBox,
-} from 'react-instantsearch-dom';
 import SEO from 'react-seo-component';
-import styled from 'styled-components';
-import gitHubCorner from '../../static/github-corner-right.svg';
-import algoliaLogo from '../../static/search-by-algolia-light-background.svg';
+import { Dump } from '../components/dump';
+import { GitHubCorner } from '../components/github-corner';
 import { Layout } from '../components/layout';
 import { H3 } from '../components/page-elements';
-import { SheetPreview } from '../components/sheet-preview';
 import { SocialButtons } from '../components/social-buttons';
 import { useSiteMetadata } from '../hooks/useSiteMetadata';
 
-const searchClient = algoliasearch(
-  process.env.GATSBY_ALGOLIA_APP_ID,
-  process.env.GATSBY_ALGOLIA_SEARCH_ONLY_KEY
-);
-
-const StyledHits = styled(Hits)`
-  li {
-    list-style: none;
-  }
-  margin: 0;
-  padding: 0;
-`;
-
-const Box = styled(SearchBox)`
-  position: relative;
-  input {
-    width: 100%;
-    height: 50px;
-    border-radius: 5px;
-    border: solid 1px lightgray;
-    font-size: 30px;
-    font-family: ${props => props.theme.h1};
-  }
-  button {
-    display: none;
-  }
-`;
-
-const AlgoliaLogo = styled.div`
-  position: relative;
-  top: 0;
-  left: 0;
-  height: 30px;
-  width: 100%;
-  background-image: url(${algoliaLogo});
-  background-repeat: no-repeat;
-  background-position: right;
-`;
-
-const GitHubCorner = styled.a`
-  position: absolute;
-  top: 0;
-  right: 0;
-  height: 80px;
-  width: 80px;
-  background-image: url(${gitHubCorner});
-  /* background-repeat: no-repeat; */
-  /* background-position: right; */
-`;
-
-export default () => {
+export default ({ data }) => {
   const {
     description,
     image,
     title,
-    gitHubUrl,
     siteUrl,
     siteLanguage,
     siteLocale,
@@ -87,23 +29,30 @@ export default () => {
         siteLocale={siteLocale}
         twitterUsername={twitterUsername}
       />
-      <GitHubCorner
-        role="link"
-        aria-label="github repo link"
-        href={gitHubUrl}
-        target="_blank"
-        rel="noopener"
-      />
+      <GitHubCorner />
       <SocialButtons />
       <H3>{description}</H3>
-      <InstantSearch
-        searchClient={searchClient}
-        indexName={process.env.GATSBY_ALGOLIA_INDEX_NAME}
-      >
-        <Box />
-        <AlgoliaLogo />
-        <StyledHits hitComponent={SheetPreview} />
-      </InstantSearch>
+      <Dump datas={data} />
     </Layout>
   );
 };
+
+export const indexQuery = graphql`
+  {
+    allMdx {
+      nodes {
+        frontmatter {
+          title
+          createdDate
+          updatedDate
+          published
+          cover
+        }
+        headings {
+          value
+          depth
+        }
+      }
+    }
+  }
+`;

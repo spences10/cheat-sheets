dd4f3d3314c78300bb04cb63ea27f40bbae26540
spences10 <spences10apps@gmail.com>
2019-11-22 15:47:57 +0000

    adjust colour

--- src/components/md-page-elements/styles.js
+++ src/components/md-page-elements/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import 'victormono';
 
 export const InlineCode = styled.code`
-  background-color: #fafafa;
+  padding: 0 3px;
+  background-color: #e1e1e1;
   font-family: 'Victor Mono', 'Courier New', Courier, monospace;
 `;

bc9a5945d8e7c70213d06b871627efc940177579
spences10 <spences10apps@gmail.com>
2019-12-11 13:23:09 +0000

    inline styles for covers

--- src/printer-components/sheet.js
+++ src/printer-components/sheet.js
@@ -4,13 +4,41 @@ export default ({ frontmatter: { title } }) => {
   return (
     <div
       style={{
+        position: 'relative',
         width: '800px',
         height: '400px',
-        border: '5px solid black',
-        padding: '1rem',
+        margin: '10px',
+        border: '7px solid rebeccapurple',
+        borderRadius: '10px',
       }}
     >
-      <h1>{title}</h1>
+      <h1
+        style={{
+          fontFamily: '"Josefin Slab"',
+          fontSize: '75px',
+          fontWeight: '700',
+          color: 'rebeccapurple',
+          textTransform: 'capitalize',
+          margin: '30px',
+          display: 'inline-block',
+          lineHeight: '1',
+        }}
+      >
+        {title}
+      </h1>
+      <h3
+        style={{
+          fontFamily: 'Maitree',
+          fontSize: '45px',
+          color: 'rebeccapurple',
+          textTransform: 'capitalize',
+          position: 'absolute',
+          bottom: '0',
+          margin: '30px',
+        }}
+      >
+        cheat sheets
+      </h3>
     </div>
   );
 };

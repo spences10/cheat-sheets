72638e6d90c681824efee1f5a2c67fdff729d2b0
spences10 <spences10apps@gmail.com>
2020-05-16 10:42:42 +0100

    redo search

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
-import { graphql, Link } from 'gatsby';
-import React, { useState } from 'react';
-import Highlighter from 'react-highlight-words';
-import SEO from 'react-seo-component';
-import styled from 'styled-components';
-import { GitHubCorner } from '../components/github-corner';
-import { Layout } from '../components/layout';
-import { SocialButtons } from '../components/social-buttons';
-import { useSiteMetadata } from '../hooks/useSiteMetadata';
+import { graphql, Link } from 'gatsby'
+import React, { useState } from 'react'
+import SEO from 'react-seo-component'
+import styled from 'styled-components'
+import { GitHubCorner } from '../components/github-corner'
+import { Layout } from '../components/layout'
+import { SocialButtons } from '../components/social-buttons'
+import { useSiteMetadata } from '../hooks/useSiteMetadata'
 
 const StyledInput = styled.input`
   width: 100%;
@@ -19,23 +18,23 @@ const StyledInput = styled.input`
   &:focus {
     outline: 3px dashed ${({ theme }) => theme.primaryAccent};
   }
-`;
+`
 
 const SheetTitle = styled.p`
   font-family: ${({ theme }) => theme.h1};
   font-size: 32px;
   font-weight: 700;
   margin: 25px 0;
-`;
+`
 
 const StyledP = styled.p`
   font-family: ${({ theme }) => theme.p};
-`;
+`
 
 const StyledDescription = styled.p`
   font-family: ${({ theme }) => theme.p};
   font-size: 26px;
-`;
+`
 
 export const StyledLink = styled(props => <Link {...props} />)`
   color: ${({ theme }) => theme.fontDark};
@@ -49,21 +48,21 @@ export const StyledLink = styled(props => <Link {...props} />)`
   &:active {
     color: ${({ theme }) => theme.secondary};
   }
-`;
+`
 
 const LinkTitle = styled(StyledLink)`
   text-decoration: underline;
   &:hover {
     text-decoration: none;
   }
-`;
+`
 
 const LinkLink = styled(StyledLink)`
   text-decoration: none;
   &:hover {
     text-decoration: underline;
   }
-`;
+`
 
 export default ({ data }) => {
   const {
@@ -74,30 +73,42 @@ export default ({ data }) => {
     siteLanguage,
     siteLocale,
     twitterUsername,
-  } = useSiteMetadata();
-  const { nodes } = data.allMdx;
-  const [searchTerm, setSearchTerm] = useState('');
-  const handleChange = event => {
-    setSearchTerm(event.target.value);
-  };
+  } = useSiteMetadata()
 
-  function filterBy(data, searchTerm) {
-    const hasSearchTerm = value =>
-      value.toLowerCase().includes(searchTerm);
+  const allSheets = data.allMdx.nodes
+  const emptyQuery = ''
 
-    return data.reduce((matches, sheet) => {
-      const headings = sheet.tableOfContents.items.filter(
-        ({ title }) => hasSearchTerm(title)
-      );
-      return [sheet.frontmatter.title, sheet.fields.slug].some(
-        hasSearchTerm
-      ) || headings.length
-        ? matches.concat(Object.assign(sheet, { headings }))
-        : matches;
-    }, []);
+  const [state, stateSet] = useState({
+    filteredData: [],
+    query: emptyQuery,
+  })
+
+  const handleInputChange = e => {
+    const query = e.target.value
+    const sheets = data.allMdx.nodes || []
+
+    const filteredData = sheets.filter((sheet, i) => {
+      const {
+        frontmatter: { title },
+        tableOfContents: { items },
+      } = sheet
+
+      const toc = items.map(item => {
+        return item.title
+      })
+      return (
+        title.toLowerCase().includes(query.toLowerCase()) ||
+        (toc &&
+          toc.join('').toLowerCase().includes(query.toLowerCase()))
+      )
+    })
+
+    stateSet({ query, filteredData })
   }
 
-  const result = filterBy(nodes, searchTerm);
+  const { filteredData, query } = state
+  const hasSearchResults = filteredData && query !== emptyQuery
+  const sheets = hasSearchResults ? filteredData : allSheets
 
   return (
     <Layout>
@@ -118,65 +129,36 @@ export default ({ data }) => {
         aria-label="search input box"
         type="text"
         placeholder="Search"
-        value={searchTerm}
-        onChange={handleChange}
+        onChange={handleInputChange}
       />
-      {result.length === 0 ? (
-        <SheetTitle>
-          Nothing for that term.
-          <span role="img" aria-label="cry emoji">
-            😭
-          </span>
-        </SheetTitle>
-      ) : (
-        result.map(({ id, frontmatter, headings, fields }) => {
-          return (
-            <div key={id}>
-              <SheetTitle>
-                <LinkTitle
-                  to={fields.slug}
-                  aria-label={`Link for ${frontmatter.title}`}
-                >
-                  <Highlighter
-                    searchWords={[searchTerm]}
-                    autoEscape={true}
-                    textToHighlight={frontmatter.title}
-                    className="highlighted"
-                  >
-                    {frontmatter.title}
-                  </Highlighter>
-                </LinkTitle>
-              </SheetTitle>
-              {headings.map(h => {
-                return (
-                  <StyledP key={`${id}${h.title}`}>
-                    <LinkLink
-                      to={`${fields.slug}${h.url}`}
-                      aria-label={`Link for ${frontmatter.title} detailing ${h.title}`}
-                    >
-                      <Highlighter
-                        searchWords={[searchTerm]}
-                        autoEscape={true}
-                        textToHighlight={h.title}
-                        className="highlighted"
-                      >
-                        {h.title}
-                      </Highlighter>
-                    </LinkLink>
-                  </StyledP>
-                );
+
+      {sheets.map(post => {
+        const {
+          id,
+          fields: { slug },
+          frontmatter: { title, date },
+          tableOfContents: { items },
+        } = post
+
+        return (
+          <article key={id}>
+            <Link to={slug}>
+              <h1>{title}</h1>
+              <p>{date}</p>
+              {items.map(item => {
+                return <p>{item.title}</p>
               })}
-            </div>
-          );
-        })
-      )}
+            </Link>
+          </article>
+        )
+      })}
     </Layout>
-  );
-};
+  )
+}
 
 export const indexQuery = graphql`
   {
-    allMdx(sort: { fields: fields___slug, order: ASC }) {
+    allMdx(sort: { fields: frontmatter___title, order: ASC }) {
       nodes {
         id
         frontmatter {
@@ -193,4 +175,4 @@ export const indexQuery = graphql`
       }
     }
   }
-`;
+`

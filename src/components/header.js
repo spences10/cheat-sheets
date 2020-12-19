dda8512e26e6cc11270b1c95fa1518eda87f7844
spences10 <spences10apps@gmail.com>
2020-12-19 12:25:06 +0000

    style header and layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,15 @@
+import { Box } from '@chakra-ui/react'
 import React from 'react'
-import styled from 'styled-components'
-import { StyledLink } from '../pages'
-import { H1 } from './md-page-elements'
-
-const HeaderWrapper = styled.header`
-  color: ${({ theme }) => theme.colors.blue[500]};
-  position: relative;
-  margin: 30px 0;
-`
+import { A } from '../components/md-page-elements'
 
 export const Header = ({ title }) => {
   return (
-    <HeaderWrapper>
-      <H1>
-        <StyledLink to="/">{title}</StyledLink>
-      </H1>
-    </HeaderWrapper>
+    <Box as="header" m="30px 0">
+      <A to="/">
+        <Box as="h1" fontSize="4xl">
+          {title}
+        </Box>
+      </A>
+    </Box>
   )
 }

2b5ee44b51fdaf548768f16d7d542ffcb2c452bc
spences10 <spences10apps@gmail.com>
2020-12-19 14:21:39 +0000

    Add Chakra to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,15 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Link } from '@chakra-ui/react'
+import { Link as GatsbyLink } from 'gatsby'
 import React from 'react'
-import { A } from '../components/md-page-elements'
 
 export const Header = ({ title }) => {
   return (
     <Box as="header" m="30px 0">
-      <A to="/">
+      <Link as={GatsbyLink} to="/">
         <Box as="h1" fontSize="4xl">
           {title}
         </Box>
-      </A>
+      </Link>
     </Box>
   )
 }

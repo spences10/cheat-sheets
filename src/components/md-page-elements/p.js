dd474cd47bf7ef9506ab474ecd109d1bbabb79a6
spences10 <spences10apps@gmail.com>
2020-12-19 11:05:36 +0000

    add page elements

--- src/components/md-page-elements/p.js
+++ src/components/md-page-elements/p.js
@@ -1,26 +1,11 @@
+import { Text } from '@chakra-ui/react'
 import React from 'react'
-import styled from 'styled-components'
-import { inlineCode } from './inline-code'
-
-const StyledText = styled.p`
-  font-size: ${({ theme }) => theme.fontSize.base};
-  margin-bottom: ${({ theme }) => theme.spacing[6]};
-  strong {
-    font-weight: 500;
-  }
-  em {
-    font-style: italic;
-    code {
-      ${inlineCode}
-    }
-  }
-  img {
-    width: 100%;
-  }
-  word-break: break-word;
-`
 
 export const P = props => {
   const { children, ...rest } = props
-  return <StyledText {...rest}>{children}</StyledText>
+  return (
+    <Text color="gray.900" fontSize="xl" {...rest}>
+      {children}
+    </Text>
+  )
 }

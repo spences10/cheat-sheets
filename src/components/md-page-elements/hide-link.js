4c7aae6b1130041064317d4a068e7db71c308db2
spences10 <spences10apps@gmail.com>
2020-12-20 10:54:15 +0000

    use color mode on link svg

--- src/components/md-page-elements/hide-link.js
+++ src/components/md-page-elements/hide-link.js
@@ -1,20 +1,31 @@
+import { useColorModeValue } from '@chakra-ui/react'
 import styled from '@emotion/styled'
+import React from 'react'
 
-export const HideLink = styled.span`
-  a {
-    float: left;
-    margin-left: -25px;
-  }
-  svg {
-    visibility: hidden;
-    height: 45px;
-    width: 20px;
-  }
-  &:hover {
+export const HideLink = props => {
+  const color = useColorModeValue(
+    '#1A202C',
+    'rgba(255, 255, 255, 0.92)'
+  )
+  const HideLinkStyle = styled.span`
+    a {
+      float: left;
+      margin-left: -25px;
+    }
     svg {
-      visibility: visible;
+      visibility: hidden;
       height: 45px;
       width: 20px;
     }
-  }
-`
+    &:hover {
+      svg {
+        visibility: visible;
+        height: 45px;
+        width: 20px;
+        fill: ${color};
+      }
+    }
+  `
+
+  return <HideLinkStyle {...props}>{props.children}</HideLinkStyle>
+}

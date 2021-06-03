30b99a6836912098738037bb8d14c1b30bc373b3
spences10 <spences10apps@gmail.com>
2021-06-03 06:46:09 +0100

    style: :lipstick: style Header

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,5 +1,7 @@
-<h1
-  class="text-4xl font-bold my-8 underline hover:no-underline hover:text-purple-600"
->
-  <a href="/">Cheat Sheets</a>
-</h1>
+<header>
+  <p
+    class="text-4xl font-bold my-8 underline hover:no-underline hover:text-purple-600"
+  >
+    <a href="/">Cheat Sheets</a>
+  </p>
+</header>

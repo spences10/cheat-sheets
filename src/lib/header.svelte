dcaa9a08776110dfaba3cd07193a0879d4163767
spences10 <spences10apps@gmail.com>
2021-06-03 05:53:23 +0100

    chore: :arrow_up: Upgrade dependencies

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,3 +1,5 @@
-<h1 class="text-4xl my-8">
-  <a href="/">Cheat Sheets </a>
+<h1
+  class="text-4xl font-bold my-8 underline hover:no-underline hover:text-purple-600"
+>
+  <a href="/">Cheat Sheets</a>
 </h1>

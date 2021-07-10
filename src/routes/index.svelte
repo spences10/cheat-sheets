6541fcbba83e0ba4cac440fe2af9064879c848af
spences10 <spences10apps@gmail.com>
2021-07-10 18:05:45 +0100

    feat: :sparkles: add fusejs for search

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,18 +19,45 @@
 </script>
 
 <script>
+  import Fuse from 'fuse.js'
   export let posts
+
+  let options = {
+    keys: ['title'],
+    includeScore: true,
+    includeMatches: true,
+    threshold: 0.2,
+  }
+  let fuse = new Fuse([...posts], options)
+  let query = ''
+  $: results = fuse.search(query)
 </script>
 
-<ul>
-  {#each posts as post}
-    {#if post.published}
+<input type="text" bind:value={query} class="border" />
+
+{#if results.length === 0}
+  <ul>
+    {#each posts as post}
+      {#if post.published}
+        <li
+          class="text-4xl font-medium my-5 hover:underline hover:text-purple-600"
+        >
+          <a sveltekit:prefetch href={`/${post.slug}`}>{post.title}</a
+          >
+        </li>
+      {/if}
+    {/each}
+  </ul>
+{:else}
+  {#each results as result}
+    <ul>
       <li
         class="text-4xl font-medium my-5 hover:underline hover:text-purple-600"
       >
-        <a sveltekit:prefetch href={`/${post.slug}`}>{post.title}</a>
+        <a sveltekit:prefetch href={result.item.slug}>
+          {result.item.title}
+        </a>
       </li>
-    {/if}
-    <pre>{JSON.stringify(post, null, 2)}</pre>
+    </ul>
   {/each}
-</ul>
+{/if}

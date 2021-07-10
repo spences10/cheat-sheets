c9f57bad320419753091817872407bf8bf8fa910
spences10 <spences10apps@gmail.com>
2021-07-10 16:03:15 +0100

    feat: :sparkles: add adpater vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
+import adapter from '@sveltejs/adapter-vercel'
 import { mdsvex } from 'mdsvex'
-import { mdsvexConfig } from './mdsvex.config.js'
 import preprocess from 'svelte-preprocess'
+import { mdsvexConfig } from './mdsvex.config.js'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,6 +15,7 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
+    adapter: adapter(),
   },
 }
 

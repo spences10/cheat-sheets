39cc70a194ac13278ae686c4bc4f451e6c8079d0
spences10 <spences10apps@gmail.com>
2021-07-11 16:37:49 +0100

    feat: :sparkles: add toable of contents component

---
+++ src/lib/table-of-contents.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let headings
+</script>
+
+{#if headings.length}
+  <aside class="">
+    <h3 class="font-extrabold text-lg mb-2" id="table-of-contents">
+      Table of Contents
+    </h3>
+    <ul class="overflow-auto h-40">
+      {#each headings as heading}
+        <li key={`heading-${heading.href}`}>
+          <a href={heading.href}>{heading.label}</a>
+        </li>
+      {/each}
+    </ul>
+  </aside>
+{/if}

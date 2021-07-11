201652b628d15ef09457414126102347cf6c9cec
spences10 <spences10apps@gmail.com>
2021-07-11 18:48:09 +0100

    feat: :lipstick: add styles for toc and scrollbars

--- src/lib/table-of-contents.svelte
+++ src/lib/table-of-contents.svelte
@@ -3,13 +3,15 @@
 </script>
 
 {#if headings.length}
-  <aside class="">
+  <aside
+    class="table-of-contents fixed right-5 w-64 lg:block hidden shadow rounded-md"
+  >
     <h3 class="font-extrabold text-lg mb-2" id="table-of-contents">
       Table of Contents
     </h3>
-    <ul class="overflow-auto h-40">
+    <ul class="overflow-auto h-72">
       {#each headings as heading}
-        <li key={`heading-${heading.href}`}>
+        <li class="mb-2" key={`heading-${heading.href}`}>
           <a href={heading.href}>{heading.label}</a>
         </li>
       {/each}

d2bec0ae34d52a79bc611a70463d6c95bf0784f5
spences10 <spences10apps@gmail.com>
2021-07-13 09:06:14 +0100

    feat: :sparkles: use fathom client

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,21 @@
 <script>
+  import { browser } from '$app/env'
+  import { page } from '$app/stores'
   import Header from '$lib/header.svelte'
+  import * as Fathom from 'fathom-client'
   import 'prism-themes/themes/prism-night-owl.css'
   import { onMount } from 'svelte'
   import { themeChange } from 'theme-change'
   import '../styles.postcss'
+
+  // https://mattjennings.io/blog/how-to-use-fathom-analytics-with-sveltekit
   onMount(async () => {
     themeChange(false)
+    Fathom.load('WERPEKMT', {
+      url: 'https://4pvafmq.cheatsheets.xyz/script.js',
+    })
   })
+  $: $page.path, browser && Fathom.trackPageview()
 </script>
 
 <div class="container mx-auto max-w-3xl px-4 sm:px-6 lg:px-8">

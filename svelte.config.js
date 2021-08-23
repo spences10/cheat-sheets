aced837627cef527389d84a603c0715af96199eb
spences10 <spences10apps@gmail.com>
2021-08-23 18:11:28 +0100

    chore: :wrench: update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -20,3 +20,7 @@ const config = {
 }
 
 export default config
+// Workaround until SvelteKit uses Vite 2.3.8 (and it's confirmed to fix the Tailwind JIT problem)
+const mode = process.env.NODE_ENV
+const dev = mode === 'development'
+process.env.TAILWIND_MODE = dev ? 'watch' : 'build'

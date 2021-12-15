24c70308feda7bd197288d3a0d0a9d24942eb96a
spences10 <spences10apps@gmail.com>
2021-12-15 09:19:58 +0000

    chore: :wrench: adpater vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
+import adapter from '@sveltejs/adapter-vercel'
 import { mdsvex } from 'mdsvex'
-import mdsvexConfig from './mdsvex.config.js'
 import preprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-auto'
+import mdsvexConfig from './mdsvex.config.js'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {

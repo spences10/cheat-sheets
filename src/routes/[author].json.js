05882f5957c3a0c1484ded96bd9b80b62b2d73a4
spences10 <spences10apps@gmail.com>
2022-04-08 18:46:36 +0100

    feat: :sparkles: add author endpoint

---
+++ src/routes/[author].json.js
@@ -0,0 +1,15 @@
+export const get = async req => {
+  const { author } = req.params
+  try {
+    const res = await fetch(`https://api.github.com/users/${author}`)
+    return {
+      status: 200,
+      body: await res.json(),
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      body: { error: error.message },
+    }
+  }
+}

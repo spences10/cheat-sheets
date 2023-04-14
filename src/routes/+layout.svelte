e3ffbf318e5cb305f167ac7f6063f46294db4d28
spences10 <spences10apps@gmail.com>
2023-04-14 09:34:00 +0100

    style: :lipstick: stick footer to page bottom

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,17 @@
-<script>
+<script lang="ts">
 	import { browser } from '$app/environment'
 	import { page } from '$app/stores'
 	import {
 		PUBLIC_FATHOM_ID,
 		PUBLIC_FATHOM_URL,
 	} from '$env/static/public'
-	import Footer from '$lib/components/footer.svelte'
-	import Header from '$lib/components/header.svelte'
+	import { Footer, Header } from '$lib/components'
 	import * as Fathom from 'fathom-client'
 	import 'prism-themes/themes/prism-night-owl.css'
 	import { onMount } from 'svelte'
 	import { themeChange } from 'theme-change'
 	import '../app.css'
 
-	// https://mattjennings.io/blog/how-to-use-fathom-analytics-with-sveltekit
 	onMount(() => {
 		themeChange(false)
 		Fathom.load(PUBLIC_FATHOM_ID, {
@@ -21,10 +19,13 @@
 			excludedDomains: ['localhost'],
 		})
 	})
-	$: $page.pathname, browser && Fathom.trackPageview()
+
+	$: $page.url.pathname, browser && Fathom.trackPageview()
 </script>
 
-<div class="container mx-auto max-w-3xl px-4 sm:px-6 lg:px-8">
+<div
+	class="container mx-auto max-w-3xl px-4 sm:px-6 lg:px-8 flex-grow"
+>
 	<Header />
 	<main>
 		<slot />

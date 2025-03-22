cdb95b63fb20c1298592e29695b63a6828e4c823
spences10 <spences10apps@gmail.com>
2025-03-22 12:15:07 +0000

    chore: :arrow_up: update deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 import autolinkHeadings from 'rehype-autolink-headings';
@@ -32,9 +32,6 @@ const config = {
 	],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 	},
 
